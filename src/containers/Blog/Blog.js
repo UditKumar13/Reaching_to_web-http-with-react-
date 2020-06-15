eaab794d79d472c5e3f7ffedfcbab733a3183b5d
Uditkumar13 <uditkumar2401@gmail.com>
2020-06-15 12:27:12 +0530

    Transforming the data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,7 +15,15 @@ class Blog extends Component {
     componentDidMount(){
 axios.get ('https://jsonplaceholder.typicode.com/posts')
 .then(response=>{
-    this.setState({posts:response.data});
+
+    const posts=response.data.slice(0,4);
+    const updatedPosts = posts.map(post=>{
+        return {
+            ...post,
+            author:'Uk'
+        }
+    });
+    this.setState({posts:updatedPosts});
     //console.log(response);
 });
     }
@@ -23,7 +31,8 @@ axios.get ('https://jsonplaceholder.typicode.com/posts')
     render () {
 
         const posts = this.state.posts.map(post=>{
-            return <Post title={post.title} key={post.id}/>;
+            return <Post title={post.title} key={post.id}
+            author={post.author}/>;
         });
 
         return (
