b1a4b6d1025814de2cac5012ed31edbaed8543ad
Uditkumar13 <uditkumar2401@gmail.com>
2020-07-03 17:41:43 +0530

    Rendering the routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import {Route} from 'react-router-dom';
+
 
 
 class Blog extends Component {
@@ -24,7 +26,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                <Route path="/" exact render={()=><h1>Home</h1>} />
+                <Route path="/"  render={()=><h1>Home2</h1>} />
+                
             </div>
         );
     }
